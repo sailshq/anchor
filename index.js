a9e8a27c6895d8673bce855448b5efd83d0d75f2
Mike McNeil <michael.r.mcneil@gmail.com>
2013-01-20 13:33:49 -0600

    Added support for using npm test

--- index.js
+++ index.js
@@ -25,22 +25,35 @@ Anchor.prototype.rules = {
 	'undefined'	: _.isUndefined,
 
 	'string'	: _.isString,
+	'alpha'		: function (x){ return check(x).isAlpha();},
+	'numeric'	: function (x){ return check(x).isNumeric();},
+	'alphanumeric'	: function (x){ return check(x).isAlphanumeric();},
 	'email'		: function (x){ return check(x).isEmail();},
+	'url'		: function (x){ return check(x).isUrl();},
+	'urlish'	: /^\s([^\/]+\.)+.+\s*$/g,
+	'ip'		: function (x){ return check(x).isIP(); },
 	'creditcard': function (x){ return check(x).isCreditCard();},
 	'uuid'		: function (x, version){ return check(x).isUUID(version);},
 
+	'int'		: function (x) { return check(x).isInt(); },
 	'integer'	: function (x) { return check(x).isInt(); },
 	'number'	: _.isNumber,
 	'finite'	: _.isFinite,
+
+	'decimal'	: function (x) { return check(x).isDecimal(); },
+	'float'		: function (x) { return check(x).isDecimal(); },
 
 	'falsey'	: function (x) { return !x; },
 	'truthy'	: function (x) { return !!x; },
 	'null'		: _.isNull,
 
 	'boolean'	: _.isBoolean,
+
 	'array'		: _.isArray,
 
-	'date'		: _.isDate
+	'date'		: _.isDate,
+	'after'		: function (x,date) { return check(x).isAfter(date); },
+	'before'	: function (x,date) { return check(x).isBefore(date); }
 
 };
 
