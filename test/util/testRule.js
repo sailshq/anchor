2cf9c47356db0959501237117eead03ec85a6b48
Mike McNeil <michael.r.mcneil@gmail.com>
2013-03-12 04:19:28 -0500

    Improved tests to verify callback-driven and callback-less each time a test is run.

--- test/util/testRule.js
+++ test/util/testRule.js
@@ -1,11 +1,24 @@
 var anchor = require('../../index.js');
+var async = require('async');
 
+// Test a rule given an deliberate example and nonexample
+// Test WITH and WITHOUT callback
 module.exports = function testRule(rule, example, nonexample) {
+
+	// Throw an error if there's any trouble
+	// (not a good production usage pattern-- just here for testing)
+	withoutCallback(rule,example,nonexample);
+	withCallback(rule,example,nonexample);
+
+};
+
+
+// Without callback
+function withoutCallback(rule, example, nonexample) {
 	var err = false;
 
 	// Should not throw error
 	anchor(example).to(rule);
-
 	
 	// Should throw error
 	try {
@@ -23,4 +36,31 @@ module.exports = function testRule(rule, example, nonexample) {
 		console.error('rule', rule);
 		throw new Error(err);
 	}
-};
+}
+
+// With callback
+function withCallback(rule, example, nonexample) {
+
+	async.series([
+		// Should not throw error
+		function (cb) {
+			anchor(example).to(rule, cb);
+		},
+
+		// Should throw error
+		function (cb) {
+			anchor(nonexample).to(rule, function (err) {
+				if (!err) cb('Invalid input (' + nonexample + ') allowed through as a ' + rule + '.');
+				else cb();
+			});
+		}], 
+
+	function (err) {
+		if(err) {
+			console.error('*****************');
+			console.error('nonexample', nonexample);
+			console.error('rule', rule);
+			throw new Error(err);
+		}
+	});
+}
