2d3cf38f65889e7d6646359b65d68f0f09a2ecc9
Scott Gress <scottmgress@gmail.com>
2016-12-21 13:51:48 -0600

    Streamline ruleset to just a few basic rules + custom
    Dumped type rules and a bunch of obscure ones.  Starting with 1.0 you can do these yourself by using `custom` and passing in a function.

--- test/miscellaneousRules.test.js
+++ test/miscellaneousRules.test.js
@@ -6,144 +6,112 @@ var testRules = require('./util/testRules.js');
 
 describe('miscellaneous rules', function() {
 
-  describe ('max/min',function () {
-    it (' should support "max" rule ', function () {
+  describe('max/min', function() {
+    it(' should support "max" rule ', function() {
       return testRules({
         max: 3
-      },2,5);
+      }, 2, 5);
+    });
+    it(' should support "min" rule ', function() {
+      return testRules({
+        min: 3
+      }, 5, 2);
+    });
+    describe(' should support both "max" and "min" rules at the same time ', function() {
+      it('with a val < min', function() {
+        return testRules({
+          min: 3,
+          max: 5
+        }, 4, 2);
+      });
+      it('with a val > max', function() {
+        return testRules({
+          min: 3,
+          max: 5
+        }, 4, 6);
+      });
     });
   });
 
-  describe ('greaterThan/lessThan',function () {
-    it (' should support "greaterThan" rule ', function () {
+  describe('maxLength/minLength', function() {
+    it(' should support "maxLength" rule ', function() {
       return testRules({
-        greaterThan: 3.5
-      }, 4, 3);
+        maxLength: 3
+      }, 'abc', 'abcde');
     });
-    it (' should support "greaterThan" rule ', function () {
+    it(' should support "minLength" rule ', function() {
       return testRules({
-        greaterThan: 3.5
-      }, 3.6, 3.5);
+        minLength: 3
+      }, 'abc', 'ab');
     });
-    it (' should support "lessThan" rule ', function () {
+    describe(' should support both "maxLength" and "minLength" rules at the same time ', function() {
+      it('with a val < minLength', function() {
+        return testRules({
+          minLength: 3,
+          maxLength: 5
+        }, 'abcd', 'ab');
+      });
+      it('with a val > maxLength', function() {
+        return testRules({
+          minLength: 3,
+          maxLength: 5
+        }, 'abcd', 'abcdef');
+      });
+    });
+  });  
+
+  describe('isURL', function() {
+
+    it('should support "isURL" rule with no options', function() {
       return testRules({
-        lessThan: 3.5
-      }, 3, 4);
+        isURL: true
+      }, 'http://sailsjs.org', 'sailsjs');
     });
-    it (' should support "lessThan" rule ', function () {
+
+    it('should support "isURL" rule with options', function() {
       return testRules({
-        lessThan: 3.5
-      }, 3.4, 3.5);
-    });
-  });
-
-  describe('url', function () {
-
-    it ('should support "url" rule with no options', function () {
-      return testRules({ url: true }, 'http://sailsjs.org', 'sailsjs');
-    });
-
-    it ('should support "url" rule with options', function () {
-      return testRules({ url: { require_protocol: true } }, 'http://sailsjs.org', 'www.sailsjs.org');
-    });
-  });
-
-  describe('before/after date', function () {
-    it (' should support "before" rule ', function () {
-      return testRules({
-        before: new Date()
-      },new Date(Date.now() - 100000),new Date(Date.now() + 1000000));
-    });
-  });
-
-  describe('required', function () {
-    it(' should support "required" with boolean', function() {
-      testRules({
-        type: 'boolean',
-        required: true
-      }, true, undefined);
-    });
-
-    it(' should support "required" with boolean value false', function() {
-      testRules({
-        type: 'boolean',
-        required: true
-      }, false, null);
-    });
-
-    it(' should support "required" with integer value 0', function() {
-      testRules({
-        type: 'integer',
-        required: true
-      }, 0, NaN);
-    });
-
-    it(' should support "required" with string', function() {
-      testRules({
-        type: 'string',
-        required: true
-      }, 'some', '');
-    });
-
-    it(' should support "required" with empty object', function() {
-      testRules({
-        type: 'json',
-        required: true
-      }, {}, undefined);
-    });
-
-    it(' should support "required" with array', function() {
-      testRules({
-        type: [],
-        required: true
-      }, ['one'], []);
-    });
-  });
-
-  describe('geojson', function () {
-    var jsondef = {
-      type: 'json',
-      geojson: true
-    };
-    var stringdef = {
-      type: 'string',
-      geojson: true
-    };
-    var validGeoJson = [
-      {
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "coordinates": [125.6, 10.1]
-        },
-        "properties": {
-          "name": "Dinagat Islands"
+        isURL: {
+          require_protocol: true
         }
-      }
-    ];
-
-    it(' should support "json" type', function () {
-      testRules(jsondef, validGeoJson[0], { foo: 'bar' });
-    });
-    it(' should support "string" type', function () {
-      testRules(stringdef, JSON.stringify(validGeoJson[0]), JSON.stringify({ foo: 'bar' }));
+      }, 'http://sailsjs.org', 'www.sailsjs.org');
     });
   });
 
-  describe('dbType', function () {
-    it(' should support "dbType" with existing validation rule', function() {
-      testRules({
-        type: 'float',
-        dbType: 'float'
-      }, 10.9, 'hi');
-    });
-
-    it(' should support "dbType" with non-existing validation rule', function() {
-      testRules({
-        type: 'float',
-        dbType: 'age'
-      }, 10, 'hi');
+  describe('isBefore/isAfter date', function() {
+    it(' should support "before" rule ', function() {
+      return testRules({
+        isBefore: new Date()
+      }, new Date(Date.now() - 100000), new Date(Date.now() + 1000000));
     });
   });
+
+  describe('custom rule', function() {
+    it ('should support a custom rule as a function', function() {
+      return testRules({
+        custom: function(x) {
+          return x.indexOf('foo') === 0;
+        }
+      }, 'foobar', 'notfoobar');
+    });
+  });
+
+
+  describe('isNotIn rule', function() {
+    it ('should support isNotIn', function() {
+      return testRules({
+        isNotIn: ['foo','bar','baz']
+      }, 'bloop', 'foo');
+    });
+  });
+  
+
+  describe('regex rule', function() {
+    it ('should support regex', function() {
+      return testRules({
+        regex: /^\w\d{2}!$/
+      }, 'a12!', 'a1goop!');
+    });
+  });
+
 
 });
