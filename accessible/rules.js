9eaf2bd3c8cc81581c18c43c1be549d68c55ac2f
Scott Gress <scottmgress@gmail.com>
2016-12-21 13:52:12 -0600

    Exposed set of allowable rules as `accessible/rules`
    So you can do `require('anchor/accessible/rules')` to get the list.

---
+++ accessible/rules.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'custom': true,
+  'isAfter': true,
+  'isBefore': true,
+  'isCreditCard': true,
+  'isEmail': true,
+  'isHexColor': true,
+  'isIn': true,
+  'isInteger': true,
+  'isIP': true,
+  'isNotIn': true,
+  'isURL': true,
+  'isUUID': true,
+  'max': true,
+  'min': true,
+  'maxLength': true,
+  'minLength': true,
+  'regex': true 
+};
